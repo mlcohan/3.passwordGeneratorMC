8614a243add2a5685d9df334794f783c55ca4e6a
Mary Cohan <mary.cohan@gmail.com>
2021-01-11 21:06:10 -0800

    setting up user input

--- script.js
+++ script.js
@@ -10,5 +10,19 @@ function writePassword() {
 
 }
 
+//Getting user input
+function generatePassword() {
+  var userPasswordLenth = prompt("How many characters would you like your password? (8-128 characters")
+  var userLowerCase = confirm("Do you want uppercase letters?")
+  var userUpperCase = prompt("Do you want lowercase letters")
+  var userNumberic = prompt("Do you want to use any numerical values")
+  var userSpecialCharacters = prompt("Do you want to use any special Characters?"?")
+}
+
+//Using user input 
+
+
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword) ;
+
+
