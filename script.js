08566b58eb426f2b1df87b0c7de0004ed38697ea
Mary Cohan <mary.cohan@gmail.com>
2021-01-12 18:34:21 -0800

    changed string to booleans, some help from TA

--- script.js
+++ script.js
@@ -24,16 +24,17 @@ function generatePassword() {
 
 
   //got from internet 
+          //length of password
      var length = userPasswordLenth,
-         lowercase = "abcdefghijklmnopqrstuvwxyz",
-         uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+         lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
+         uppercase = ["A", "B","C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
          numbers = "0123456789",
          specialchar = "!@#" //add more
-         retVal = "";
-      for (var i = 0, n = lowercase+uppercase+numbers+specialchar.length; i < length; ++i) {
-         retVal += charset.charAt(Math.floor(Math.random() * n));
-      }
-    // return retVal; //whats retVal do 
+         finalPassword = [];
+     // for (var i = 0, n = length; i < length; ++i) {
+     //    retVal += lowercase+uppercase+numbers+specialchar.charAt(Math.floor(Math.random() * n));
+     // }
+    // return retVal; //whats retVal do, i REPLACED IT W PASSWORD
 
   //Password Length
   var userPasswordLenth = prompt("How many characters would you like your password? (8-128 characters")
@@ -43,28 +44,59 @@ function generatePassword() {
       }
     else{
       
-      password.length === userPasswordLenth
+      password.length === userPasswordLenth // is this redundant??
 
     } 
   //Upper Case letters y/n
   var userLowerCase = confirm("Do you want lowercase letters?")
 
     if (userLowerCase) { 
-      password
+      lowercase === true //this is a guess
     ;   //how make password use lowercase
     }
     else{
   
-    password.length === userPasswordLenth
+      lowercase === false
 
     }   
 
   var userUpperCase = confirm("Do you want uppercase letters")
+
+    if (userUpperCase) { 
+    uppercase === true //this is a guess
+    ;   //how make password use lowercase
+    }
+    else{
+    uppercase === false
+    }
+
   var userNumberic = confirm("Do you want to use any numerical values")
+
+    if (userNumeric) { 
+    numbers === true //this is a guess
+    ;   //how make password use lowercase
+    }
+    else{
+
+    numbers === false;
+    }
   var userSpecialCharacters = confirm("Do you want to use any special Characters?")
 
+  if (userSpecialCharacters) { 
+    specialchar === true //this is a guess
+  ;   //how make password use lowercase
+  }
+  else{
 
-window.alert(password)
+    specialchar === false;
+  }
+
+  for (var i = 0, n = password.length; i < passwordLength; ++i) {
+    password += passowrd.charAt(Math.floor(Math.random() * n));
+  } 
+
+  return password; //whats return retVal do, i REPLACED IT W PASSWORD
+ 
   
 }
 
