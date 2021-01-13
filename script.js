02d6e95be62dbe7f13d60da2409bcbc99fbddeb5
Mary Cohan <mary.cohan@gmail.com>
2021-01-12 16:59:06 -0800

    gave error message if invalid length

--- script.js
+++ script.js
@@ -10,13 +10,38 @@ function writePassword() {
 
 }
 
+
+//should i be declaring the variables 
+var userPasswordLenth
+var userLowerCase
+var userUpperCase
+var userNumberic
+var userSpecialCharacters
+
+
 //Getting user input
 function generatePassword() {
   var userPasswordLenth = prompt("How many characters would you like your password? (8-128 characters")
+
+    if (userPasswordLenth < 8 || userPasswordLenth > 128) 
+      { window.alert("Invalid entry"); userPasswordLenth;  //how can i make it repeat the q
+      }
+    else{
+      
+      password.length === userPasswordLenth
+
+    } 
+
   var userLowerCase = confirm("Do you want uppercase letters?")
+
+    
+
   var userUpperCase = prompt("Do you want lowercase letters")
   var userNumberic = prompt("Do you want to use any numerical values")
-  var userSpecialCharacters = prompt("Do you want to use any special Characters?"?")
+  var userSpecialCharacters = prompt("Do you want to use any special Characters?")
+
+
+
 }
 
 //Using user input 
