36cac41c4971a388fa605e74ff745b744b01c4ab
Mary Cohan <mary.cohan@gmail.com>
2021-01-13 18:45:24 -0800

    working on creating the password string

--- script.js
+++ script.js
@@ -8,21 +8,105 @@ function writePassword() {
 
   passwordText.value = password;
 
+  passwordText.innerHTML=text;
+//text is not being grabbed .......
 }
+
 
 //Getting user input
 function generatePassword() {
-  var userPasswordLenth = prompt("How many characters would you like your password? (8-128 characters")
-  var userLowerCase = confirm("Do you want uppercase letters?")
-  var userUpperCase = prompt("Do you want lowercase letters")
-  var userNumberic = prompt("Do you want to use any numerical values")
-  var userSpecialCharacters = prompt("Do you want to use any special Characters?"?")
+
+          //variable for parts of password
+     var length = userPasswordLength, //necessary?
+         lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
+         uppercase = ["A", "B","C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+         numbers = "0123456789".split(" "); //does this work instead of quotes around the whole thing?
+         specialchar = "!@#" //add more
+         text = "";
+     // for (var i = 0, n = length; i < length; ++i) {
+     //    retVal += lowercase+uppercase+numbers+specialchar.charAt(Math.floor(Math.random() * n));
+     // }
+    // return retVal; //whats retVal do, i REPLACED IT W PASSWORD
+
+  //Password Length
+  var userPasswordLength = prompt("How many characters would you like your password? (8-128 characters")
+
+    if (userPasswordLength < 8 || userPasswordLength > 128) { 
+      window.alert("Invalid entry"); 
+        return  //this is new, see if this works
+      }
+    else{
+      
+      text.length === userPasswordLength 
+
+    } 
+  //Upper Case letters y/n
+  var userLowerCase = confirm("Do you want lowercase letters?")
+
+    if (userLowerCase) { 
+      lowercase === true 
+    ;   
+    }
+    else{
+  
+      lowercase === false
+
+    }   
+
+  var userUpperCase = confirm("Do you want uppercase letters")
+
+    if (userUpperCase) { 
+    uppercase === true 
+    ;   
+    }
+    else{
+    uppercase === false
+    }
+
+    //numerical
+  var userNumeric = confirm("Do you want to use any numerical values")
+
+    if (userNumeric) { 
+    numbers === true 
+    ;   
+    }
+    else{
+
+    numbers === false;
+    }
+  var userSpecialCharacters = confirm("Do you want to use any special Characters?")
+
+  if (userSpecialCharacters) { 
+    specialchar === true //this is a guess
+  ;   //how make password use lowercase
+  }
+  else{
+
+    specialchar === false;
+  }
+
+ 
+
+  for (var i = 0, i = password.length; i < length; i++) {
+    password += password.charAt(Math.floor(Math.random() * i));
+    console.log(password)
+  } 
+  //somehow need to be filling a string concat??
+
+
+  // return password; // ends function and returns the password?? not working
+ 
+  
 }
 
-//Using user input 
-if (128 <  userPasswordLenth < 8)
+
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword) ;
+generateBtn.addEventListener("click", writePassword) 
 
+
+// for( var i=0; i < IdLength.value; i++ ){
+//   text += shuffle.charAt(Math.floor(Math.random() * shuffle.length));
+// }
+//  Results.innerHTML=text;
 
